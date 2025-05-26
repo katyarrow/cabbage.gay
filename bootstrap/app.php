caed9dd93b96db82f051f13a50e87265442afb47
Kat Yarrow <kat.yarrow@proton.me>
2025-05-26 16:49:21 +0100

    Notify admins if there is an error on the system.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Notifications\AdminErrorNotification;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -14,5 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->report(function(Exception $exception) {
+            Notification::route('mail', config('app.admin_notification_email'))
+                ->notify(new AdminErrorNotification($exception));
+        });
     })->create();
