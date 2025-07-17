98a746725e49bec52e5a3ab0890e0530f50a8333
Kat Yarrow <kat.yarrow@proton.me>
2025-07-18 00:05:24 +0100

    Added test for releases page.

---
+++ tests/Browser/ReleasesPageTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Browser;
+
+use Laravel\Dusk\Browser;
+use Tests\Browser\Components\Footer;
+use Tests\Browser\Components\Header;
+use Tests\DuskTestCase;
+
+class ReleasesPageTest extends DuskTestCase
+{
+    public function test_releases_page_loads(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/releases');
+            $browser->assertHasHeaderAndFooter();
+            $browser->assertTitle('Releases - cabbage.gay');
+            $browser->assertSee('Releases');
+        });
+    }
+}
