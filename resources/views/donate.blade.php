3f52caae2cdc870c5a0f930141183f471b5a355c
Kat Yarrow <kat.yarrow@proton.me>
2025-07-18 01:30:10 +0100

    Fix width issue on donate page.

--- resources/views/donate.blade.php
+++ resources/views/donate.blade.php
@@ -2,10 +2,10 @@
     title="Donate"
     description="Enjoying {{ config('app.name') }}? Consider donating to help with continued development and hosting."
 >
-    <div class="text-center max-w-2xl mx-auto">
+    <div class="text-center mx-auto">
         <h1 class="text-3xl font-semibold tracking-wider mb-5">Make a Donation</h1>
 
-        <p class="text-lg">
+        <p class="text-lg max-w-2xl mx-auto">
             Hosting, maintaining, and adding features to {{ config('app.name') }} takes effort and money.
             If you are enjoying the site and are able to contribute then any amount is appreciated. If you are
             not able to contribute (or just don't want to) then don't worry, we aim to keep this site free for
@@ -13,7 +13,7 @@
         </p>
         @if (config('donate.monero_address'))
             <h2 class="text-xl font-semibold tracking-wider mt-10">Monero</h2>
-            <p>Address: {{ config('donate.monero_address') }}</p>
+            <p class="break-all">Address: {{ config('donate.monero_address') }}</p>
         @endif
         @if (config('donate.email_address'))
             <h2 class="text-xl font-semibold tracking-wider mt-10">Other Ways</h2>
